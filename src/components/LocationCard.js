88e14f5657dd65cd3053d563f4b06f721aca0326
naan <adnansislam@protonmail.com>
2021-02-08 21:15:14 -0500

    Create LocationCard component

---
+++ src/components/LocationCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LocationCard.css';
+
+class LocationCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return(
+      <div className='locationCard'>
+      </div>
+    );
+  }
+}
+
+export default LocationCard;
--- src/components/WaitTimes.js
+++ src/components/WaitTimes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './WaitTimes.css';
+import LocationCard from './LocationCard';
 
 class WaitTimes extends React.Component {
   constructor(props) {
@@ -16,6 +17,19 @@ class WaitTimes extends React.Component {
       <section className='waitTimeSection'>
         <div className='waitTimeContainer'>
           <h6>Wait Times as of {this.state.time} {this.state.date}</h6>
+          {/* TODO: Create locationCards using data and loop */}
+          {/* TODO: Create 'dummy' cards with 0 height  to maintain spacing when numCards % 3 != 0 */}
+          <div className='locationCardContainer'>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+            <LocationCard location='196 Albany' time='0 - 30 minutes' lastReported='10:30 am'></LocationCard>
+          </div>
         </div>
       </section>
     );
