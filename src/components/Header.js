2da105a6d238b1ab2fda3445f4a017561af117f8
naan <adnansislam@protonmail.com>
2021-02-12 18:55:12 -0500

    Add menu icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars} from '@fortawesome/free-solid-svg-icons'
 import './Header.css'
+
+function ToggleMenu(e){
+  e.preventDefault();
+  console.log("Hello!");
+}
 
 function Header(){
   return (
     <header>
       <div className='headerContainer'>
         <h1>NYC health and hospitals <br></br> covid 19 testing wait times</h1>
+        <FontAwesomeIcon className='menuIcon' icon={faBars} onClick={ToggleMenu}/>
       </div>
     </header>
   );
