174f6baeb5147f8c47222f10a07805127d35bd20
naan <adnansislam@protonmail.com>
2021-02-11 20:54:36 -0500

    Add icon and text inside seearch bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch} from '@fortawesome/free-solid-svg-icons'
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
@@ -9,6 +11,12 @@ class SearchBar extends React.Component {
   render() {
     return(
       <div className='searchBar'>
+        <div className='searchIconContainer'>
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+        <div className='searchInputContainer'>
+          <input type='text' placeholder='Search'></input>
+        </div>
       </div>
     );
   }
