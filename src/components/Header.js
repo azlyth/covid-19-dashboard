a7950ba996813f67abb370e561261489704c4f4f
naan <adnansislam@protonmail.com>
2021-02-12 19:46:11 -0500

    Add click handler to menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,39 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars} from '@fortawesome/free-solid-svg-icons'
-import './Header.css'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
 
-function ToggleMenu(e){
-  e.preventDefault();
-  console.log("Hello!");
-}
 
-function Header(){
-  return (
-    <header>
-      <div className='headerContainer'>
-        <h1>NYC health and hospitals <br></br> covid 19 testing wait times</h1>
-        <FontAwesomeIcon className='menuIcon' icon={faBars} onClick={ToggleMenu}/>
-      </div>
-    </header>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showLinks: false};
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    console.log('click');
+    this.setState(state => ({
+      showLinks: !state.showLinks
+    }));
+  }
+  
+  render() {
+    return (
+      <header className={this.state.showLinks ? 'showLinks' : ''}>
+        <div className={'headerContainer' + (this.state.showLinks ? (' showLinks') : '')}>
+          <div className='headerTextContainer'>
+            <h1>NYC health and hospitals <br></br> covid 19 testing wait times</h1>
+          </div>
+          <div className='menuContainer'>
+            <FontAwesomeIcon className='menuIcon' icon={this.state.showLinks ? faTimes : faBars} onClick={this.toggleMenu}/>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
